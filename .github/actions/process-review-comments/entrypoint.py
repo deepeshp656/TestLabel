f16c6e4b61edd9cb6c16136fae66124836f15500
DeepeshHomex <75995267+DeepeshHomex@users.noreply.github.com>
2021-02-09 04:01:19 +0530

    Update entrypoint.py

--- .github/actions/process-review-comments/entrypoint.py
+++ .github/actions/process-review-comments/entrypoint.py
@@ -12,17 +12,23 @@ def setup_args():
 
     return parser.parse_args()
 
-def review_comment_check(comment_body):
+def review_comment_check(comment_body, pull_request_review_id):
     print(comment_body)
     res = comment_body.startswith(tuple(pref_list)) 
     print(res)
+    if res == 'False':
+        print("comenting")
+        review_comment_reply(pull_request_review_id)
     
     return res
+
 def review_comment_reply(pull_request_review_id):
     url = "https://api.github.com/repos/octocat/hello-world/pulls/{}/comments/{}/replies".format(repository_name, pr, pull_request_review_id)
     headers = { 'Accept': 'application/vnd.github.v3+json' }
     body = "Your review comment does not follow review etiquette "
     resp = requests.get(url=url, headers=headers, body=body)
+    
+    print("commented")
     
 
 
@@ -49,8 +55,7 @@ def main():
     
     for comment in data:
         if "in_reply_to_id" not in comment:
-            if review_comment_check(comment['body']) == 'False':
-                review_comment_reply(comment['pull_request_review_id'])
+            if review_comment_check(comment['body'], comment['pull_request_review_id']):
                 print(comment['path'])
     # Creates an API object
 
