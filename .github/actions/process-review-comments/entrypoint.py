4d313b323a0647ce99deb39c80e969ae88b17b1b
DeepeshHomex <75995267+DeepeshHomex@users.noreply.github.com>
2021-02-09 04:13:01 +0530

    Update entrypoint.py

--- .github/actions/process-review-comments/entrypoint.py
+++ .github/actions/process-review-comments/entrypoint.py
@@ -22,8 +22,8 @@ def review_comment_check(comment_body):
 def review_comment_reply(pull_request_review_id):
     url = "https://api.github.com/repos/{}/pulls/{}/comments/{}/replies".format(repository_name, pr, pull_request_review_id)
     headers = { 'Accept': 'application/vnd.github.v3+json' }
-    body = "Your review comment does not follow review etiquette "
-    resp = requests.post(url=url, headers=headers, body=body)
+    payload = {'body': 'Your review comment does not follow review etiquette'}
+    resp = requests.post(url=url, headers=headers, payload=payload)
     
     print("commented")
     
