81e808a5f5e28ee19da2ba77113f1e9d339cd8ac
DeepeshHomex <75995267+DeepeshHomex@users.noreply.github.com>
2021-02-08 23:51:14 +0530

    Update review.sh

--- .github/script/review.sh
+++ .github/script/review.sh
@@ -1,5 +1,5 @@
 curl \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/deepeshp656/TestLabel/pulls/54/comments
+  https://api.github.com/repos/deepeshp656/TestLabel/pulls/"$PR_NUMBER"/comments
 
 echo "54"
