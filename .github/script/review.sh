95f2118486b34a063a1bf647badadaaf983c0aae
DeepeshHomex <75995267+DeepeshHomex@users.noreply.github.com>
2021-02-09 21:44:19 +0530

    Update review.sh

--- .github/script/review.sh
+++ .github/script/review.sh
@@ -1,6 +1,7 @@
   
   curl \
   -X POST \
+  -H "authorization: Bearer $GH_TOKEN" \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/deepeshp656/TestLabel/pulls/"$PR_NUMBER"/comments/572382525/replies \
   -d '{"body":"Your review comment does not follow review etiquette"}'
@@ -8,9 +9,17 @@
 
 curl \
   -X PATCH \
+  -H "authorization: Bearer $GH_TOKEN" \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/deepeshp656/TestLabel/pulls/comments/572382704 \
   -d '{"body":"adding reply"}'
+  
+curl \
+  -X POST \
+  -H "authorization: Bearer $GH_TOKEN" \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/deepeshp656/TestLabel/issues/56/comments\
+  -d '{"body":"body"}'
 
 
 
