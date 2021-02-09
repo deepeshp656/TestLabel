67b81afb2cc2a2f2173ceeaa0fc01e0cb00a624a
DeepeshHomex <75995267+DeepeshHomex@users.noreply.github.com>
2021-02-09 19:46:26 +0530

    Update review.sh

--- .github/script/review.sh
+++ .github/script/review.sh
@@ -1,5 +1,21 @@
 curl \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/deepeshp656/TestLabel/pulls/"$PR_NUMBER"/comments
+  
+  
+  curl \
+  -X POST \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/deepeshp656/TestLabel/pulls/"$PR_NUMBER"/comments/572382525/replies \
+  -d '{"body":"Your review comment does not follow review etiquette"}'
+
+
+curl \
+  -X PATCH \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/deepeshp656/TestLabel/pulls/comments/572382704 \
+  -d '{"body":"adding reply"}'
+
+
 
 echo "54"
