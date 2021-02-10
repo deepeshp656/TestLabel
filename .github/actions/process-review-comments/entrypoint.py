a0af3aded086ecc02c3a3d09fd7855343db98c71
DeepeshHomex <75995267+DeepeshHomex@users.noreply.github.com>
2021-02-11 01:12:33 +0530

    Create entrypoint.py

--- .github/actions/process-review-comments/entrypoint.py
+++ .github/actions/process-review-comments/entrypoint.py
@@ -3,14 +3,14 @@ import argparse
 import os
 import requests
 import json
-from difflib import get_close_matches
 from urllib.parse import urlparse
 
 
 def setup_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--token", help="A GitHub token for the repo")
-    parser.add_argument("--pr-number", help="get the PR number")
+    parser.add_argument("--ref", help="get the git ref")
+    
 
     return parser.parse_args()
 
@@ -19,18 +19,11 @@ def review_comment_check(comment_body):
 
     return comment_body.startswith(tuple(pref_list))
 
-def fuzzy_review_comment_check(comment_body):
-    first_word = comment_body.split()[0]
-    match = get_close_matches(first_word, pref_list)
-    if not match:
-        return true
 
-
-
-def parse_review_comment(data, github):
+def parse_review_comment(data):
     for comment in data:
         if "in_reply_to_id" not in comment:
-            if not fuzzy_review_comment_check(comment["body"]):
+            if not review_comment_check(comment["body"]):
                 review_comment_edit(comment["id"], github, comment["body"])
 
 
@@ -44,12 +37,13 @@ def review_comment_edit(id, github, body):
     }
     print(headers)
     payload = {
-        "body": "⚠️  review etiquette not followed on ⚠️  :  "
+        "body": "⚠️  review etiquette not followed on : "
         + str(body)
         + " \n \n For more information please visit : https://github.com/HomeXLabs/reviewington/blob/main/docs/pr_etiquette.md "
     }
     resp = requests.patch(url=url, headers=headers, data=json.dumps(payload))
 
+    print(resp)
 
 
 def main():
@@ -72,7 +66,9 @@ def main():
     args = setup_args()
     github = args.token
     global pr
-    pr = args.pr_number
+    git_ref= args.ref
+    pr = git_ref.split("/")[2]
+    
 
     url = "https://api.github.com/repos/{}/pulls/{}/comments".format(
         repository_name, pr
@@ -82,7 +78,7 @@ def main():
 
     resp = requests.get(url=url, headers=headers)
     data = resp.json()
-    parse_review_comment(data, github)
+    parse_review_comment(data)
 
 
 if __name__ == "__main__":
